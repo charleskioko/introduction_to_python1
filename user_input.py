c202bcc5587d86d252c8e1ebe3209cca46bdf9ab
charleskioko <kioko27c@gmail.com>
2024-10-29 16:02:42 +0300

    python assignment 1

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Asking for user input
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("Where do you live? ")
+
+# Printing a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
